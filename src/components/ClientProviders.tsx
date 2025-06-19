4d9eac230daad6c70254fabf187d63241ee7ae2b
Mohamed Draou <mohamedaminedraou@gmail.com>
2025-06-19 20:29:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react';
 import { RepairProvider } from '@/context/RepairContext';
 import { InventoryProvider } from '@/context/InventoryContext';
 import { ClientProvider } from '@/context/ClientContext'; // Import ClientProvider
-import { Toaster } from '@/components/ui/toaster';
+import { Toaster } from '@/components/ui/sonner';
 
 export function ClientProviders({ children }: { children: ReactNode }) {
   return (
